4973163c522ba7a69f4bdf6e9bf3c3e795a04711
Antun <antun.jerkovic@gmail.com>
2022-08-30 20:04:47 +0200

    dk

--- 05_BranchingMergingPullRequestsInGH/02_AnOverviewOfBranches.py
+++ 05_BranchingMergingPullRequestsInGH/02_AnOverviewOfBranches.py
@@ -2,5 +2,5 @@
 
 # Tradicionaly: Diverging from main-line, Often very expensive(copy of source dir)
 # In git i GH: Lightweight, Fast, Encouraged to be used, Works zbog nacina rada git-a (snapshots-samo se promjene dosnimavaju a neizmjenjeni se predstavljaju sa shortcutima)
-# Commits in Git: sastoje se od pontera na snapshot, email-a, i comit-poruke, pintera na prethodni komit
-# Branch in Git: pointer na kommit; defbranch je master(main) i ponta na zadnji komit; koristimo branch za nove features, ispravljanje bug-ova, eksperimente; HEAD- special pointer koji prati aktivnu granu i  prelazi na drugu sa "checkout"
+# Commits in Git: sastoje se od pontera na snapshot, email-a, i comit-poruke, pointera na prethodni komit
+# Branch in Git: pointer na kommit; defbranch je master(main) i pointa na zadnji komit; koristimo branch za nove features, ispravljanje bug-ova, eksperimente; HEAD- special pointer koji prati aktivnu granu i  prelazi na drugu sa "checkout"
--- 05_BranchingMergingPullRequestsInGH/06_PullRequestsAndGitHubFlow.py
+++ 05_BranchingMergingPullRequestsInGH/06_PullRequestsAndGitHubFlow.py
@@ -6,4 +6,4 @@
 
 # GH ima dosta osobina koje poticu kreiranje pull-reqsa kao i omogucavaju colaboratorima evaluaciju promjena koje pull-req donosi
 
-# Vise od pola videa gill trosi da jos jednom kaze kako feture-branch sluzi za popravke ili promjene koje se onda kroz pull-req zajednicki pregledaju i kad se odobre mergaju u main branch a branch se onda moze obrisati!
+# Vise od pola videa gill trosi da jos jednom kaze kako feture-branch sluzi za popravke ili promjene koje se onda kroz pull-req zajednicki pregledaju i kad se odobre mergaju u main branch a branch se onda moze obrisati! Obicno faze u zivotu brancha: kreiranje, commiti, pullreq, review, merge, brisanje
