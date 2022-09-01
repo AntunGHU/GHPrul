633f86f6074f95a8e5f97d556993aa8840075177
Antun <antun.jerkovic@gmail.com>
2022-09-01 19:44:25 +0200

    dk

--- 07_SocialAspectOfGitHub/05_DemoWorkingWithPullRequests.py
+++ 07_SocialAspectOfGitHub/05_DemoWorkingWithPullRequests.py
@@ -2,8 +2,17 @@
 
 # Kreiranje PRQ, Requestianje review-anja, merganje PRQ-a
 
-# Klik na PRQ (tab pored Code), Klik "New Pull Request" ( dobijam obavjest da je Automatski merge moguc i Compare tool), Compare pa "Create pull request",
+# Klik na PRQ (tab pored Code), Klik "New Pull Request"(odmah ispod naslova "Compare changes" pojavljuje se kombinatorkika koju granu u koji repo koju granu - biramo base fork AntunGHU) ( dobijam obavjest da je Automatski merge moguc i Compare tool), Compare pa "Create pull request",
 
 # Pojavljuje se "Open a pull rew" gdje dajem ime i klik "Create PRQ" i to je to s te strane.
 
-# Sad je na orginalnom repou (AntunGHU) da vidi PRQ i dalje po tom radi (3'00 u videu)
+# Sad je na orginalnom repou (AntunGHU) da vidi PRQ i dalje po tom radi: neko sugerira izmjene. Klikamo na ime predlozene izmjene
+# ?  Create readme.md #1
+# ? Open
+# ? EvicaGHU wants to merge 2 commits into AntunGHU:master from EvicaGHU:master
+# i dobijamo pregled comita od EvicaGHU, filese editane
+# Ako se slazemo s prijedlozima kliknemo na "Merge pull req" kad ce PRQ biti inkorporiran u nas main, a ako ne onda unosimo koment i bouncamo back!
+
+# Bouncani back koment stize i emailom na adresu evicae sa sadrzajem komentara kada evica ide u svoj gh i radi dalje na kodu! Nakon dorade jos jedan commit i zaista se to vidi u otvorenom PRQ-u kod AntunGHU.
+
+# Konacno zadovoljan AntunGHU merga! klikom na "Merge pull req" pa "Confirm merge" i merganje se odradjuje a Evica prima mail od AntunGHU o merganju njenog PRQ-a
