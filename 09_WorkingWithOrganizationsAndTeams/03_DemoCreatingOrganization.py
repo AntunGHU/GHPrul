72bfb8b9bbb313ef767131f2e8d6dc62a713e4d9
Antun <antun.jerkovic@google.com>
2022-09-02 14:38:16 +0200

    dp

--- 09_WorkingWithOrganizationsAndTeams/03_DemoCreatingOrganization.py
+++ 09_WorkingWithOrganizationsAndTeams/03_DemoCreatingOrganization.py
@@ -1,1 +1,21 @@
 # 7'19
+
+# Setupanje Organizacije from scratch i dodavanje usera u nju
+
+# Settings/Organizations: I sad imamo opciju kreiranja "New organization" ili "Turn AntunGHU into an organization" (ali obzirom da sam od prije kreirao organizaciju EvicaDOO koje sam clan, kraj ove opcije pise "You cannot transform this account into an organization until you leave all organizations that you’re a member of.")
+
+# KL: "New organization" i naravno, MS je odmah uveo novosti tako da nudi Free ali i 2 tipa za placanja (ipak i gill to ima!). Biram Free i ulazim u Gill's sucelje:  Ime organizacije, kontaktni email, Pripadnost ove organizacije(to personal account ili Bussiness institution), Verifying (imao spiralnu galaksiju) i Next
+
+# Dolazimo do "Wellcome AntunDOO" i search-anja clanova "Search by username, full name or email address" ali to mozemo za sada preskociti! Kako god dodjemo do "Wellcome to GitHub" ("Woohoo! You've joined millions of developers who are doing their best work on GitHub. Tell us what you're interested in. We'll help you get there.")
+
+# U welcome ima gomila pitanja: (gill je odmah dobio base interface of organization): What do you spend time on most day-to-day? ; How many people do you expect to actively work within this GitHub organization? ; What type of work do you plan to use this organization for? ; What do you expect to do on GitHub in the next seven days? ; Do you have an existing repository for your project?
+
+# Nakon odgovaranja i Submitanja --> "We think you’re gonna like it here." pojavljuje se i meni baseview od AntunDOO sa karticama unutar kojih je i teams("Overview Repositories Projects Packages Teams People2"). Takodjer je i gomila taskova kojih bi po GH trebalo napraviti: Pocinje sa "Invite your people"
+
+# Pocinjem sa "Invite EvicaGHU to AntunDOO" i predlazem joj da bude clan (moze biti i owner tako da bude više ownera!!!; Members ne mogu pozivati druge ili mjenjati sttingse!!) i KL "Send invitation". Otvaram EvicaGHU i tamo pod organization (vidi se tocka pokraj Your organization u Yourprofile padajucem izborniku) vidim invitation- klikam na join! --> "You’ve been invited to the AntunDOO organization!" i ponovo KL na "Join AntunDOO"
+
+# Sad kad imamo 2 clana kreiramo novi repo: sve slicno kao i u pojedinacnom pristupu! Osim kreiranja novog repoa ima mogucnost da transferiramo postojeci individualni repo u organizaciju: cinimo to!: Settings/DangerZone/Transfer pwnership (upisujemo ime repoa, lozinka usera i repo vise nije dio mog accounta - postao je dio AntunDOO)
+
+# Prelazim u AntunDOO i vidim da sad imam 2 repoa!
+
+# Settingsi organizacije: Puno postavki!!! Gill se dohvatio Verified domains, AuditLogs-a,
